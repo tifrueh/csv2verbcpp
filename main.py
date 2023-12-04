6f2fda16300e28794b7ad76f7bacd68c8d67c1da
Timo Früh <timo.frueh@icloud.com>
2023-12-04 22:23:20 +0100

    Add comments to main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import argparse
 from pathlib import Path
 
+# Create parser object
 parser = argparse.ArgumentParser(
         prog="csv2verbcpp",
         description="Convert a csv to .verb.cpp files for Conjugateur",
 )
 
+# Add parser arguments
 parser.add_argument("filepath", 
                     help="Path to the csv file"
 )
@@ -14,4 +16,6 @@ parser.add_argument("-d", "--directory",
                     help="Directory to put the .verb.cpp files in [default: .]"
 )
 
+# Parse the arguments
 args = parser.parse_args()
+
